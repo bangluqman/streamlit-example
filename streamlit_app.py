ff90318bf1d12db76607c0ea4711013884339095
bangluqman <bang.luqman@gmail.com>
2024-05-13 10:23:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,36 +5,11 @@ import streamlit as st
 
 """
 # Aplikasi Text Summary
-Aplikasi untuk merangkup artike.
+Aplikasi untuk merangkum artikel.
 """
 
 txtInput = st.text_area("Masukkan Text disini...")
-bt = st.buttun("Ringkas Artikel")
+bt = st.button("Ringkas Artikel")
 txtOutput = st.text_area()
 
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
